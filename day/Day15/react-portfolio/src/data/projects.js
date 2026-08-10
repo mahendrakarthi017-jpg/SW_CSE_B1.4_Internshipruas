439cfca42c69ad0a2d8f97cba58857a7a4557351
mahendrakarthi017-jpg <mahendrakarthi000@gmail.com>
2026-08-10 09:23:48 +0530

    changes

---
+++ day/Day15/react-portfolio/src/data/projects.js
@@ -0,0 +1,15 @@
+export const categories = ['all', 'web', 'fullstack', 'auth'];
+
+export const projectsData = [
+ {
+    id: 1,
+    title: 'React Portfolio',
+    description: 'Full-stack portfolio',
+    longDesc:
+      'A REACT portfolio, about me.',
+    tech: ['HTML', 'CSS', 'React', 'Node.js'],
+    category: 'fullstack',
+    featured: true,
+    liveUrl: '#',
+    githubUrl: '#'
+  },]
